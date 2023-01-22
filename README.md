38807dc3d2f8208679cedd176775b170883bf32d
allnulled <todosanulados@gmail.com>
2023-01-22 22:25:26 +0100

    readme, script, imagen

---
+++ README.md
@@ -0,0 +1,29 @@
+# infojobs-envio-automatico-de-cv
+
+Script de envío automático de CV en InfoJobs, plataforma de empleo nº1 en España con sede en países nórdicos y comandada por el holding empresarial Schibsted.
+
+## Uso
+
+**1. Instalar `Greasemonkey` o `Tampermonkey` en tu navegador**
+
+Nada que explicar, si sabes lo que te estás haciendo.
+
+**2. Agrega el script a la extensión**
+
+El script es el que está en `script-greasemonkey.js`. Cambia los textos que rellenarán los cuestionarios, si quieres.
+
+**3. Visita Infojobs**
+
+Hecho esto, cuando entres en Infojobs, el `Greasemonkey` se encargará de ejecutar los scripts adecuados en cada página. La explicación del script está entre comentarios.
+
+## Testimonio
+
+Esta es la bandeja de entrada del Gmail después de 2 horas de envío de CVs:
+
+[!Currículums enviados a Infojobs vistos desde Gmail](Infojobs-fundido-en-cosa-de-2-horas.png)
+
+También hice un vídeo, de unos 45 minutos aproximadamente. Si Github lo acepta *(ocupa 110MB)*, está en `infojobs-envio-cv-masivo.mp4`.
+
+## Conclusión
+
+No dan. Simplemente, no dan. Prefieren tener gente loca. Entre la Ley y el Libre Mercado, estoy en pelotas.
---
+++ script-greasemonkey.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name     InfoJobs: búsqueda acelerada
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+console.log("En 5 segundos empieza...");
+
+setTimeout(async function() {
+  const simplyWait = (milisegundos) => new Promise(ok => setTimeout(ok, milisegundos));
+  // 1. Si es la página del formulario, rellenar automático:
+  if(window.location.href.startsWith("https://www.infojobs.net/candidate/application/index.xhtml?")) {
+    const f = document.querySelector("form#myForm");
+    // Rellena los textareas:
+    Array.from(f.querySelectorAll("textarea")).forEach(t => t.value = "Esto es un lenguaje lógico en castellano que traduce a JavaScript: https://github.com/allnulled/castelog y esto es para montar redes de servidores https://github.com/allnulled/restologia Estoy en el pozo, ¿me puede sacar alguien, por favor?");
+    // Rellena los checkboxes:
+    Array.from(f.querySelectorAll("input[type=radio]")).forEach(i => i.click());
+    // Envía el formulario:
+    document.querySelector("button#botonEnviar").click();
+  } else {
+    // 2. Si tiene el botón de inscribirse, darle automático:
+  	try { return document.querySelector("#candidate_application_bottom").click() } catch(error) {}
+    // 3. Si es la página de ofertas y no estamos buscando nosotros (redirigido por completar formulario), cerrar la pestaña:
+    // En Firefox tendrás que ir a "about:config" y habilitar (a true) la config: dom.allow_scripts_to_close_windows
+    if(window.location.href.startsWith("https://www.infojobs.net/jobsearch/search-results/list.xhtml?")) {
+      if(window.location.href.indexOf("keyword=") === -1) {
+        window.close();
+      }
+      // 4. Si es la página de ofertas y SÍ estamos buscando nosotros, abrir todas las ofertas (paulatinamente) y clicar siguiente:
+      else {
+        // 4.1. Scrolear hasta abajo primero para que se carguen todas las ofertas:
+        for(let i=0; i<3; i++) {
+          console.log("Scrol número " + i);
+        	window.scrollTo(0, document.body.scrollHeight);
+          await simplyWait(1000);
+        }
+        let links=Array.from(document.querySelectorAll("a.ij-OfferCardContent-description-title-link"));
+        let indiceLinks = 0;
+        // 4.2. Abrir links en intervalos de 1s
+        let idInterval = setInterval(() => {
+          try {
+            const link = links[indiceLinks++];
+            console.log("Abriendo link " + indiceLinks);
+            window.open(link.href, '_blank');
+          } catch(error) {
+            clearInterval(idInterval);
+            console.log("En 7 segundos, siguiente");
+            setTimeout(() => {
+            	Array.from(document.querySelectorAll("button")).filter(b => b.textContent.trim() === "Siguiente")[0].click();
+              console.log("En 3 segundos, refrescar");
+              setTimeout(() => {
+              	window.location.reload()
+              }, 3000);
+            }, 7000);
+          }
+        }, 1000);
+      }
+    }
+    
+  }
+}, 5000);
